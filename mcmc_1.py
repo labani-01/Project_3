40a8034377aa7f3115331a62f56992369c113d2a
labani-01 <labanoy2000@gmail.com>
2023-11-20 22:16:25 -0500

    importing_data_handwritten

--- mcmc_1.py
+++ mcmc_1.py
@@ -3,10 +3,37 @@ import matplotlib.pyplot as plt
 import math
 import random
 from scipy.stats import uniform
-
-
-param = [V_0, A, t_0, s_0]
+import pandas as pd
+import csv
  
+csv_file = 'SingleEventMoyal.csv'
+
+t = []
+mV = []
+ 
+with open(csv_file, 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        t.append(row.get('time'))
+        mV.append(row.get('data_ch2'))
+
+print(t)
+## ['Apples', 'White Bread', 'Wholemeal Bread']
+
+print(mV)
+## ['A bag of 3 apples', 'A loaf of white bread', 'A loag of wholemeal bread']
+
+ 
+
+#plt.plot(t, mV)
+#plt.show() 
+ 
+
+
+
+#param = [V_0, A, t_0, s_0]
+ 
+
  
 def model(t, param):
     u = (t - param[2])/param[3] 
@@ -14,7 +41,7 @@ def model(t, param):
     return model
 
 def loglikelihood(param):
-    return - 0.5 * ((V - model(t, param)) ** 2.0).sum()
+    return - 0.5 * ((mV - model(t, param)) ** 2.0).sum()
  
 def lognormpdf(x, mean, sd):
     var = float(sd)**2
@@ -34,4 +61,12 @@ def logposterior(param):
     return loglikelihood(param) + logprior(param)
      
 
-     
+    
+def proposal(param):
+    p1 = numpy.random.normal() + param[0]
+    p2 = numpy.random.normal() + param[1]
+    p3 = numpy.random.normal() + param[2]
+    p4 = numpy.random.normal() + param[3]
+    return (p1*p2*p3*p4) 
+    
+
