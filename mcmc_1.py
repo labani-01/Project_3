2b1b04eb912420188a30569f2d0f4af84ae8dab4
labani-01 <labanoy2000@gmail.com>
2023-11-22 15:04:44 -0500

    adding changes

--- mcmc_1.py
+++ mcmc_1.py
@@ -5,22 +5,27 @@ import random
 from scipy.stats import uniform
 import pandas as pd
 import csv
+import tqdm
+from numpy import ndarray
  
 csv_file = 'SingleEventMoyal.csv'
 
-t = []
-mV = []
+arr_1 = []
+arr_2 = []
  
 with open(csv_file, 'r') as f:
     reader = csv.DictReader(f)
     for row in reader:
-        t.append(row.get('time'))
-        mV.append(row.get('data_ch2'))
+        arr_1.append(row.get('time'))
+        arr_2.append(row.get('data_ch2'))
+t = np.array(arr_1, dtype='float64')
+mV = np.array(arr_2, dtype='float64') 
 
-print(t)
+ 
+#print(t)
 ## ['Apples', 'White Bread', 'Wholemeal Bread']
 
-print(mV)
+#print(mV)
 ## ['A bag of 3 apples', 'A loaf of white bread', 'A loag of wholemeal bread']
 
  
@@ -28,20 +33,25 @@ print(mV)
 #plt.plot(t, mV)
 #plt.show() 
  
+V_0 = float(input("Enter a value for V_0 = "))
+A = float(input("Enter a value for A = "))
+t_0 = float(input("Enter a value for t_0 = "))
+s_0 = float(input("Enter a value for s_0 = "))
 
-
-
-#param = [V_0, A, t_0, s_0]
  
+ 
+#param = ndarray((4,),float)
+#param = np.array([V_0, A, t_0, s_0])
+#print(len(param))  
 
  
-def model(t, param):
-    u = (t - param[2])/param[3] 
-    model = param[1]*np.exp(-(u + np.exp(u))/2) + param[0] 
+def model(t, V_0, A, t_0, s_0):
+    u = (t - t_0)/s_0
+    model = A*np.exp(-(u + np.exp(u))/2) + V_0
     return model
 
-def loglikelihood(param):
-    return - 0.5 * ((mV - model(t, param)) ** 2.0).sum()
+def loglikelihood(V_0, A, t_0, s_0):
+    return - 0.5 * ((mV - model(t, V_0, A, t_0, s_0)) ** 2.0).sum()
  
 def lognormpdf(x, mean, sd):
     var = float(sd)**2
@@ -49,24 +59,58 @@ def lognormpdf(x, mean, sd):
     num = -(float(x)-float(mean))**2/(2*var) 
     return (num + denom)
     
-def logprior(param):
-    t_0_prior = lognormpdf(param[2], 1000, 1)
-    s_0_prior = lognormpdf(param[3], 600, 1) 
-    V_0_prior = np.log(uniform.pdf(param[0], loc=-200, scale=1000))
-    A_prior = np.uniform(uniform.pdf(param[0], loc=-2000, scale=4000))
+def logprior(V_0, A, t_0, s_0):
+    t_0_prior = lognormpdf(t_0, 1000, 1)
+    s_0_prior = lognormpdf(s_0, 600, 1) 
+    V_0_prior = np.log(uniform.pdf(V_0, loc=-200, scale=1000))
+    A_prior = np.log(uniform.pdf(A, loc=-2000, scale=4000))
     return(t_0_prior + s_0_prior + V_0_prior + A_prior)
  
  
-def logposterior(param):
-    return loglikelihood(param) + logprior(param)
+def logposterior(V_0, A, t_0, s_0):
+    return loglikelihood(V_0, A, t_0, s_0) + logprior(V_0, A, t_0, s_0)
      
 
     
-def proposal(param):
-    p1 = numpy.random.normal() + param[0]
-    p2 = numpy.random.normal() + param[1]
-    p3 = numpy.random.normal() + param[2]
-    p4 = numpy.random.normal() + param[3]
-    return (p1*p2*p3*p4) 
+def logproposal(V_0, A, t_0, s_0):
+    p1 = np.log(np.random.normal() + V_0)
+    p2 = np.log(np.random.normal() + A)
+    p3 = np.log(np.random.normal() + t_0)
+    p4 = np.log(np.random.normal() + s_0) 
+    return (p1 + p2 + p3 + p4) 
     
+def mcmc(a,b,c,d, post, prop, iterations):
+    x = [initial]
+    p = [post(x[-1])]
+    for i in tqdm.tqdm(range(iterations)):
+        x_test = prop(x[-1])
+        p_test = post(x_test)
 
+        acc = p_test / p[-1]
+        u = numpy.random.uniform(0, 1)
+        if u <= acc:
+            x.append(x_test)
+            p.append(p_test)
+    return x, p
+    
+    
+chain, prob = mcmc(V_0, A, t_0, s_0, logposterior, logproposal, 1000000)
+
+plt.figure()
+plt.title("Evolution of the walker")
+plt.plot(chain)
+plt.ylabel('t-value')
+plt.xlabel('Iteration')
+
+plt.figure()
+plt.title("Evolution of the walker")
+plt.plot(chain)
+plt.xlim(0, 100)
+plt.ylabel('t-value')
+plt.xlabel('Iteration')
+
+plt.figure()
+plt.title("Posterior samples")
+_ = plt.hist(chain[100::100], bins=100)
+
+plt.show()
---
+++ mcmc_2.py
@@ -0,0 +1,137 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import random
+from scipy.stats import uniform
+import pandas as pd
+import csv
+import tqdm
+from numpy import ndarray
+
+csv_file = 'SingleEventMoyal.csv'
+
+arr_1 = []
+arr_2 = []
+ 
+with open(csv_file, 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        arr_1.append(row.get('time'))
+        arr_2.append(row.get('data_ch2'))
+t = np.array(arr_1, dtype='float64')
+mV = np.array(arr_2, dtype='float64') 
+plt.scatter(t, mV)
+plt.show() 
+
+def model(theta, t):
+    u = (t - theta[2])/theta[3]
+    return (theta[1] * np.exp(-(u + np.exp(u))/2) + theta[0])
+    
+def loglikelihood(theta, t, mV):
+    return - 0.5 * ((mV - model(theta, t)) ** 2.0).sum()
+    
+def lognormpdf(x, mean, sd):
+    var = float(sd)**2
+    denom = np.log((2*math.pi*var)**.5)
+    num = -(float(x)-float(mean))**2/(2*var) 
+    return (num + denom)
+    
+def logprior(theta):
+    t_0_prior = 0
+    s_0_prior = 0
+    A_prior = 0 #flat prior: log(1) = 0
+    V_0_prior = 0
+    return(t_0_prior + s_0_prior + A_prior + V_0_prior)
+    
+def logposterior(theta, t, mV):
+    return logprior(theta) + loglikelihood(theta, t, mV)
+
+def run_mcmc(ln_posterior, nsteps, ndim, theta0, stepsize, args=()):
+    """
+    Run a Markov Chain Monte Carlo
+    
+    Parameters
+    ----------
+    ln_posterior: callable
+        our function to compute the posterior
+    nsteps: int
+        the number of steps in the chain
+    theta0: list
+        the starting guess for theta
+    stepsize: float
+        a parameter controlling the size of the , random step
+        e.g. it could be the width of the Gaussian distribution
+    args: tuple (optional)
+        additional arguments passed to ln_posterior
+    """
+    # Create the array of size (nsteps, ndims) to hold the chain
+    # Initialize the first row of this with theta0
+    chain = np.zeros((nsteps, ndim))
+    chain[0] = theta0
+    # Create the array of size nsteps to hold the log-likelihoods for each point
+    # Initialize the first entry of this with the log likelihood at theta0
+    log_likes = np.zeros(nsteps)
+    log_likes[0] = ln_posterior(chain[0], *args)
+    # Loop for nsteps
+    for i in tqdm.tqdm(range(1, nsteps)):
+        # Randomly draw a new theta from the proposal distribution.
+        # for example, you can do a normally-distributed step by utilizing
+        # the np.random.randn() function
+        #theta_new = chain[i - 1] + stepsize * np.random.randn(ndim)
+        theta_new = np.random.normal(ndim) + chain[i - 1]
+        # Calculate the probability for the new state
+        log_like_new = loglikelihood(theta_new, *args)
+        
+        # Compare it to the probability of the old state
+        # Using the acceptance probability function
+        # (remember that you've computed the log probability, not the probability!)
+        log_p_accept = log_like_new - log_likes[i - 1]
+        
+        # Chose a random number r between 0 and 1 to compare with p_accept
+        r = np.random.rand()
+        
+        # If p_accept>1 or p_accept>r, accept the step
+        # Else, do not accept the step
+        if log_p_accept > np.log(r):
+            chain[i] = theta_new
+            log_likes[i] = log_like_new
+        else:
+            chain[i] = chain[i - 1]
+            log_likes[i] = log_likes[i - 1]
+            
+    return chain
+
+chain = run_mcmc(logposterior, 1000000, 4, [10, 10, 11, 10], 0.001, (t, mV)) 
+fig, ax = plt.subplots(3)
+ax[0].plot(chain[:, 0])
+ax[1].plot(chain[:, 1])
+ax[2].plot(chain[:, 2]) 
+plt.show() 
+
+# Now that we've burned-in, let's get a fresh chain
+#chain = run_mcmc(logposterior, 1000000, 4, chain[-1], 0.001, (t, mV))
+#fig, ax = plt.subplots(3)
+#ax[0].plot(chain[:, 0])
+#ax[1].plot(chain[:, 1])
+#ax[2].plot(chain[:, 2]) 
+#plt.show() 
+
+
+plt.figure()
+plt.title("Evolution of the walker")
+plt.plot(chain)
+plt.ylabel('t-value')
+plt.xlabel('Iteration')
+
+plt.figure()
+plt.title("Evolution of the walker")
+plt.plot(chain)
+plt.xlim(0, 100)
+plt.ylabel('t-value')
+plt.xlabel('Iteration')
+
+plt.figure()
+plt.title("Posterior samples")
+#_ = plt.hist(chain[100::100], bins=100)
+plt.hist(chain)
+plt.show()
