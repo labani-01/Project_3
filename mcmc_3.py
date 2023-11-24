a25d07a7bafcb8f5891922c767e553441621806d
labani-01 <labanoy2000@gmail.com>
2023-11-24 13:08:27 -0500

    changes

--- mcmc_3.py
+++ mcmc_3.py
@@ -7,12 +7,13 @@ import pandas as pd
 import csv
 import tqdm
 from numpy import ndarray
-
+from scipy.stats import gamma, norm
  
 csv_file = 'SingleEventMoyal.csv'
 
 arr_1 = []
 arr_2 = []
+ev = []
  
 with open(csv_file, 'r') as f:
     reader = csv.DictReader(f)
@@ -21,82 +22,107 @@ with open(csv_file, 'r') as f:
         arr_2.append(row.get('data_ch2'))
 x = np.array(arr_1, dtype='float64')
 y = np.array(arr_2, dtype='float64') 
-u = []
-
-def coffe(theta, x):
-    v = []
-    s = 1
-    for i in range(len(x)):
-        v = u.append((x[i] - theta[2])/theta[3]) 
-        s = s * (theta[1] * np.exp(-(v + np.exp(v))/2) + theta[0])
-    return s
-    
  
-# assume unit variance normal distributions
-def loglikelihood(theta):
-    return - 0.5 * ((y - model(theta, x)) ** 2.0).sum()
-    
-def logprior(theta):
-    # Assume unormalized uniform prior
-    for p in theta:
-        if p > 20000 or p < -20000:
-            return - numpy.inf
-    return 0
-    
-def logposterior(theta):
-    return coffe(theta, x)
-    
-    
-def proposal(theta):
-    return np.random.normal(0, 1, 4) #+ theta
-    
-#theta = [1, 2, 3, 4]
-
-#z = logposterior(theta)
-#print(z) 
-
+def model(a, b, t, c):
+    u = (x - t)/c
+    mod = b * np.exp(-(u + np.exp(u))/2) + a
+    return mod
  
-    
- 
+dgamma = gamma.logpdf
+dnorm = norm.logpdf
 
-def mcmc(theta0, post, prop, iterations):
-    chain = np.zeros((iterations, 4))
-    chain[0] = theta0
-    p = np.zeros(iterations)
-    p[0] = post(chain[0])
-    for i in tqdm.tqdm(1, iterations):
-        theta_test = prop(chain[i - 1])
-        p_test = post(theta_test)
+def calc_posterior(a, b, t, c):
+    # Calculate joint posterior, given values for V_0, A, t, sigma_0
 
-        acc = p_test/p[i - 1]
-        u = np.random.uniform(0, 1)
-        if u <= acc:
-            chain[i] = theta_test
-            p[i] = p_test
-        else:
-            chain[i] = chain[i - 1]
-            p[i] = p[i - 1]
-    return chain, p
+    # Priors on a,b
+    t_0_prior = dnorm(t, 1000, 1)
+    s_0_prior = dnorm(c, 600, 1) 
+    V_0_prior = np.log(uniform.pdf(a, loc=-200, scale=1000))
+    A_prior = np.log(uniform.pdf(b, loc=-2000, scale=4000))
+    logp = t_0_prior + s_0_prior + V_0_prior + A_prior
+    #logp = dnorm(a, 0, 10000) + dnorm(b, 0, 10000) + dnorm(t, 0, 10000) + dnorm(c, 0, 10000)
+    # Calculate mu
+    u = (x - t)/c
+    mod = b * np.exp(-(u + np.exp(u))/2) + a
+    # Data likelihood
+    logp += sum(dnorm(y - mod, 0, 1)) 
+    return logp
     
     
- 
-chain, prob = mcmc([-1, 0, 10, -5], logposterior, proposal, 1000)
-print(chain)
+rnorm = np.random.normal
+runif = np.random.rand
+np.random.seed(42)
 
-plt.figure()
-plt.title("Evolution of the walker")
-plt.plot(chain)
-plt.ylabel('t-value')
-plt.xlabel('Iteration')
+def metropolis(n_iterations, initial_values, prop_var=1):
 
-plt.figure()
-plt.title("Evolution of the walker")
-plt.plot(chain)
-plt.xlim(0, 100)
-plt.ylabel('t-value')
-plt.xlabel('Iteration')
+    n_params = len(initial_values)
+            
+    # Initial proposal standard deviations
+    prop_sd = [prop_var]*n_params
+    
+    # Initialize trace for parameters
+    trace = np.empty((n_iterations+1, n_params))
+    
+    # Set initial values
+    trace[0] = initial_values
+        
+    # Calculate joint posterior for initial values
+    current_log_prob = calc_posterior(*trace[0])
+    
+    # Initialize acceptance counts
+    accepted = [0]*n_params
+    
+    for i in range(n_iterations):
+    
+        if not i%1000: print('Iteration %i' % i)
+    
+        # Grab current parameter values
+        current_params = trace[i]
+    
+        for j in range(n_params):
+    
+            # Get current value for parameter j
+            p = trace[i].copy()
+    
+            # Propose new value
+            if j==2 or j==3:
+                # Ensure mean and standard-deviation are positive
+                theta = np.exp(rnorm(np.log(current_params[j]), prop_sd[j]))
+            else:
+                theta = rnorm(current_params[j], prop_sd[j])
+            
+            # Insert new value 
+            p[j] = theta
+    
+            # Calculate log posterior with proposed value
+            proposed_log_prob = calc_posterior(*p)# Log-acceptance rate
+            alpha = proposed_log_prob - current_log_prob
+    
+            # Sample a uniform random variate
+            u = runif()
+    
+            # Test proposed value
+            if np.log(u) < alpha:
+                # Accept
+                trace[i+1,j] = theta
+                current_log_prob = proposed_log_prob
+                accepted[j] += 1
+            else:
+                # Reject
+                trace[i+1,j] = trace[i,j]
+                
+    return trace, accepted
 
-plt.figure()
-plt.title("Posterior samples")
-_ = plt.hist(chain[100::100], bins=100)
+
+n_iter = 10000
+trace, acc = metropolis(n_iter, (1,0,1,1), 0.001)
+for param, samples in zip(['intercept', 'normalization', 'mean', 'standard_deviation'], trace.T):
+    fig, axes = plt.subplots(1, 2, figsize=(8, 2))
+    axes[0].plot(samples)
+    axes[0].set_ylabel(param)
+    axes[1].hist(samples[int(n_iter/2):])
+    plt.show()
+plt.hist(trace)
 plt.show()
+
+ 
